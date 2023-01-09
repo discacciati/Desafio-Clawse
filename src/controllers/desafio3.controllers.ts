e4058eceb41d06ae0b88020f2323619bff9a4271
Eliane Discacciati <91580538+discacciati@users.noreply.github.com>
2023-01-09 12:50:07 -0300

    fix: desconstrução do request.body-

--- src/controllers/desafio3.controllers.ts
+++ src/controllers/desafio3.controllers.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import desafio3Service from "../services/desafio3.service";
 
 const desafio3Controller = async (req: Request, res: Response) => {
-  const data: object = req.body.valor;
-  const desafio3 = await desafio3Service(data);
+const { array, dir, key } = req.body.valor;
+  const desafio3 = await desafio3Service({ array, dir, key });
   return res.status(201).json(desafio3);
 };
 
